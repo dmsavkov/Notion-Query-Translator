578d340746b7fadfb5bb7b89218e4ddc38b48490
dmsavkov <dmitry_savkov@mail.ru>
2026-04-07 01:14:31 +0300

    fix: changed base image (template) to make run_code usable

--- third-party/e2b/template.py
+++ third-party/e2b/template.py
@@ -2,7 +2,7 @@ from e2b import AsyncTemplate
 
 template = (
     AsyncTemplate()
-    .from_base_image()  # Includes the code interpreter server
+    .from_template("code-interpreter-v1")
     .set_user("root")
     .run_cmd("pip3 install requests")
     .set_user("user")
