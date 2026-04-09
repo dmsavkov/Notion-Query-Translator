6ca6a76c102babcb29732bed5cd682d238fdba95
dmsavkov <dmitry_savkov@mail.ru>
2026-04-09 11:57:58 +0300

    fix: added dotenv installation

--- third-party/e2b/template.py
+++ third-party/e2b/template.py
@@ -4,6 +4,6 @@ template = (
     AsyncTemplate()
     .from_template("code-interpreter-v1")
     .set_user("root")
-    .run_cmd("pip3 install requests")
+    .run_cmd("pip3 install requests python-dotenv")
     .set_user("user")
 )
